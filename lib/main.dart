cd6a0ded63bb1143c59cdb3139e395680dcc39fd
Asher Hwang <dev@hsh.kr>
2021-05-20 10:21:44 +0900

    버튼 누르면 숫자가 올라가는 어플 제작

--- lib/main.dart
+++ lib/main.dart
@@ -6,23 +6,54 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '플러터 앱 개발하기',
-      home: Scaffold(
-        appBar: AppBar(title: Text('플러터앱 개발하기')),
-        body: Center(
-          child: Text(
-            'Hello World my name is HSH and now I write some codes. '
-            '가나다라마바사'
-            ' 안녕하세요'
-            ' 안녕하세요'
-            ' 안녕하세요'
-            ' 안녕하세요'
-            ' 안녕하세요'
-            ' 안녕하세요'
-            ' 안녕하세요',
-          ),
+      title: 'Hello flutter',
+      theme: ThemeData(primaryColor: Colors.blue),
+      home: HomePage(
+        title: 'Header title here',
+      ),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  final String title;
+
+  HomePage({Key key, this.title}) : super(key: key);
+
+  @override
+  HomePageState createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  int _count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.title)),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _increase,
+        tooltip: '숫자 올리는 버튼',
+        child: Icon(Icons.add),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('버튼을 누른 회수는'),
+            Text(
+              '$_count',
+              style: Theme.of(context).textTheme.headline1,
+            ),
+          ],
         ),
       ),
     );
+  }
+
+  _increase() {
+    setState(() {
+      _count++;
+    });
   }
 }
